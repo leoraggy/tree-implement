645d8d4a3cc82fb2f033aaa2735cb365eac340da
leoraggy <leoragual35@gmail.com>
2026-02-19 14:30:21 -0800

    Completed livecode 3

--- src/toMap.java
+++ src/toMap.java
@@ -1,4 +1,8 @@
-public class Traversal {
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class toMap {
   public static void main(String[] args) {
     TreeNode<Integer> root = new TreeNode<>(2, null, null);
     root.left = new TreeNode<>(1, null, null);
@@ -12,16 +16,35 @@ public class Traversal {
     root.right.right.right  = new TreeNode<>(8, null, null);
 
     TreeNode<String> stringRoot = new TreeNode<>("hello",null,null);
-    stringRoot.left = new TreeNode<>("how", null, null);
-    stringRoot.left.left = new TreeNode<>("are", null, null);
-    stringRoot.left.right = new TreeNode<>("you", null, null);
+    stringRoot.left = new TreeNode<>("hello", null, null);
+    stringRoot.left.left = new TreeNode<>("hello", null, null);
+    stringRoot.left.right = new TreeNode<>("hello", null, null);
 
-    stringRoot.right = new TreeNode<>("I", null, null);
+    stringRoot.right = new TreeNode<>("am", null, null);
     stringRoot.right.left = new TreeNode<>("am", null, null);
-    stringRoot.right.right = new TreeNode<>("so", null, null);
+    stringRoot.right.right = new TreeNode<>("am", null, null);
     stringRoot.right.right.right = new TreeNode<>("good", null, null);
 
-    printGreater(root);
+    // printGreater(root, 19);
+    System.out.println(toMap(stringRoot));
+  }
+
+  public static <T> Map<T, Integer> toMap(TreeNode<T> node){
+    Map<T, Integer> counts = new HashMap<>();
+    toMap(node, counts);
+    return counts;
+  } 
+
+  private static <T> void toMap(TreeNode<T> node, Map<T, Integer> counts){
+    if(node == null){
+      return;
+    }
+
+    // fill up those counts
+    counts.put(node.value, counts.getOrDefault(node.value, 0) + 1);
+
+    toMap(node.left, counts);
+    toMap(node.right, counts);    
   }
 
   public static int countNodes(TreeNode<?> node){
@@ -57,6 +80,7 @@ public class Traversal {
     postorder(node.left);
     postorder(node.right);
     System.out.print(node.value + " ");
-
   }
+
+
 }
